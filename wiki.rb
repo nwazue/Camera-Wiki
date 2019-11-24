307525b32fff751335430a2f814183b6cbc7836e
Onyee <onyeenwazue@gmail.com>
2019-11-24 15:51:59 -0500

    updates

--- wiki.rb
+++ wiki.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "uri"
 
 def page_content(title)
     File.read("pages/#{title}.txt")
@@ -16,8 +17,24 @@ get "/" do
     erb :welcome
 end
 
+get "/new" do
+    erb :new
+end
+
 get "/:title" do
     @title = params[:title]
     @content = page_content(@title)
     erb :show
 end
+
+get "/:title/edit" do
+  @title = params[:title]
+  @content = page_content(@title)
+  erb :edit
+end
+
+post "/create" do
+  save_content(params["title"], params["content"])
+  redirect URI.escape("/#{params["title"]}")
+end
+
