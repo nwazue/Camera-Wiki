4c55697b4713d0730782e2e093539423036d754e
Onyee Nwazue <53847259+nwazue@users.noreply.github.com>
2019-11-23 23:30:11 -0500

    update
    update

---
+++ wiki.rb
@@ -0,0 +1,23 @@
+require "sinatra"
+
+def page_content(title)
+    File.read("pages/#{title}.txt")
+  rescue Errno::ENOENT
+    return nil
+  end
+
+  def save_content(title, content)
+    File.open("pages/#{title}.txt", "w") do |file|
+      file.print(content)
+    end
+  end  
+
+get "/" do
+    erb :welcome
+end
+
+get "/:title" do
+    @title = params[:title]
+    @content = page_content(@title)
+    erb :show
+end
